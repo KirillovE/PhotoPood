745a361da6978bc5777a4b1274eff907afe590cc
KirillovE <evgeniy.kirillov@live.ru>
2018-09-28 22:07:11 +0300

    Добавлены визуальные элементы на экран авторизации: кнопка отмены и возврата на предыдущий экран, индикатор активности посередине экрана во время загрузки, системный индикатор активности

--- PhotoPood/LoginViewController.swift
+++ PhotoPood/LoginViewController.swift
@@ -6,20 +6,44 @@
 //  Copyright © 2018 Триада. All rights reserved.
 //
 
-import Foundation
 import WebKit
 
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        webView.navigationDelegate = self
+        showLoginScreen()
+    }
+    
+    /// Показывает экран авторизации
+    private func showLoginScreen() {
         let requests = RequestHelper()
         let loginRequest = requests.getLoginRequest()
         webView.load(loginRequest)
     }
+    
+    /// Закрывает текущий экран при нажатии на кнопку
+    ///
+    /// - Parameter sender: Нажатая и отпущенная кнопка
+    @IBAction func cancelButtonDidTap(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
 
-
+extension LoginViewController: WKNavigationDelegate {
+    
+    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = false
+        activityIndicator.stopAnimating()
+    }
+    
 }
