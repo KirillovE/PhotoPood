3569948a2f73f4bb4103e43fb0f52424c0a9e26e
KirillovE <evgeniy.kirillov@live.ru>
2018-09-28 22:08:55 +0300

    Рефакторинг, документирование и проверка необходимости показа кнопки входа. Также вызов анимации элементов перенесён в метод viewDidLoad

--- PhotoPood/StartViewController.swift
+++ PhotoPood/StartViewController.swift
@@ -12,22 +12,39 @@ class StartViewController: UIViewController {
 
     @IBOutlet weak var loginButton: UIButton!
     @IBOutlet weak var titleLabel: UILabel!
+    let needsAuthorization = true
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setAppearance()
+        showWithAnimation()
+    }
+    
+    /// Настраивает внешний вид элементов представления
+    fileprivate func setAppearance() {
         loginButton.layer.cornerRadius = loginButton.frame.height / 4
         titleLabel.alpha = 0
         loginButton.alpha = 0
     }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if !needsAuthorization {
+            self.loginButton.isHidden = true
+        }
+    }
+    
+    /// Показывает название приложения и кнопку входа с анимацией
+    private func showWithAnimation() {
         let animationsHelper = AnimationsHelper()
         
         animationsHelper.pullView(titleLabel, fromOutsideOf: view, withDirection: .down)
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-            animationsHelper.pullView(self.loginButton, fromOutsideOf: self.view, withDirection: .up)
+        if needsAuthorization {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                animationsHelper.pullView(self.loginButton, fromOutsideOf: self.view, withDirection: .up)
+            }
         }
     }
+    
 }
