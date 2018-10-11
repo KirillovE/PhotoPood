c4362df942a4c03701bac99964b0d8b955de9c10
KirillovE <evgeniy.kirillov@live.ru>
2018-10-11 23:55:00 +0300

    Создан главный экран приложения и контроллер для него

---
+++ PhotoPood/Controllers/MeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MeViewController.swift
+//  PhotoPood
+//
+//  Created by Евгений Кириллов on 11/10/2018.
+//  Copyright © 2018 Триада. All rights reserved.
+//
+
+import UIKit
+
+class MeViewController: UIViewController {
+
+    @IBOutlet weak var avatar: UIImageView!
+    @IBOutlet weak var followers: UILabel!
+    @IBOutlet weak var follows: UILabel!
+    @IBOutlet weak var table: UITableView!
+    
+}
