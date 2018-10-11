61d5d58fd63ce9160bcb32c5afd979ec512a8014
KirillovE <evgeniy.kirillov@live.ru>
2018-10-11 23:57:17 +0300

    Первоначальная настройка главного экрана

--- PhotoPood/Controllers/MeViewController.swift
+++ PhotoPood/Controllers/MeViewController.swift
@@ -15,4 +15,9 @@ class MeViewController: UIViewController {
     @IBOutlet weak var follows: UILabel!
     @IBOutlet weak var table: UITableView!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.navigationBar.prefersLargeTitles = true
+        avatar.layer.cornerRadius = avatar.frame.height / 2
+    }
 }
