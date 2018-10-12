795886f401a35df435a2e21cafbdc673ae60e5cd
KirillovE <evgeniy.kirillov@live.ru>
2018-10-13 00:30:41 +0300

    Метод получения информации о пользователе переведён на использование Decodable

--- PhotoPood/Models/APIManager.swift
+++ PhotoPood/Models/APIManager.swift
@@ -13,39 +13,38 @@ class APIManager {
     
     private let requestsHelper = RequestHelper()
     
+    /// Передаёт информацию о пользователе
+    ///
+    /// - Parameter completion: Экземпляр класса `User`
+    func getUser(completion: @escaping (User?) -> Void) {
+        guard let request = requestsHelper.getUserInfoRequest() else { return }
+        
+        load(request) { data in
+            guard let data = data else { return }
+            
+            let userContainer = try? JSONDecoder().decode(UserContainer.self, from: data)
+            completion(userContainer?.data)
+        }
+    }
+    
     /// Обращается в сеть по указанному запросу
     ///
     /// - Parameters:
     ///   - urlRequest: Запрос для обращения в сеть
     ///   - completion: Результат выполнения запроса
-    private func load(_ urlRequest: URLRequest, completion: @escaping (Any?) -> Void) {
+    private func load(_ urlRequest: URLRequest, completion: @escaping (Data?) -> Void) {
         UIApplication.shared.isNetworkActivityIndicatorVisible = true
         
         let configuration = URLSessionConfiguration.default
         let session = URLSession(configuration: configuration)
-        let task = session.dataTask(with: urlRequest) { (data, response, _) in
-            print("data:\n", data ?? "empty")
-            print("response:\n", response ?? "empty")
-            
-            UIApplication.shared.isNetworkActivityIndicatorVisible = false
+        let task = session.dataTask(with: urlRequest) { (data, _, _) in
+            completion(data)
+            DispatchQueue.main.async {
+                UIApplication.shared.isNetworkActivityIndicatorVisible = false
+            }
         }
         
         task.resume()
-    }
-    
-    /// Передаёт информацию о пользователе
-    ///
-    /// - Parameter completion: Экземпляр класса `User`
-    func getUser(completion: @escaping (User?) -> Void) {
-        guard let request = requestsHelper.getUserInfoRequest() else {
-            completion(nil)
-            return
-        }
-        
-        load(request) { json in
-            let user = User(fromDictionary: json as! [String : Any])
-            completion(user)
-        }
     }
     
 }
