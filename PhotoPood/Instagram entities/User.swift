a1f56452bb2b2a7edacfcdffc8d49511853ed5fd
KirillovE <evgeniy.kirillov@live.ru>
2018-10-13 03:30:58 +0300

    Методы переставлены по приоритетам

--- PhotoPood/Instagram entities/User.swift
+++ PhotoPood/Instagram entities/User.swift
@@ -8,6 +8,29 @@
 
 import Foundation
 
+/// Данные о пользователе социальной сети
+struct User: Decodable {
+    
+    let id: String
+    let username: String
+    let fullName: String
+    let avatarURL: String
+    let bio: String
+    let webSite: String
+    let counts: Counts
+    
+    enum CodingKeys: String, CodingKey {
+        case id
+        case username
+        case fullName = "full_name"
+        case avatarURL = "profile_picture"
+        case bio
+        case webSite = "website"
+        case counts
+    }
+    
+}
+
 /// Вспомогательная структура для счётчиков
 struct Counts: Decodable {
     let media, follows, followedBy: Int
@@ -17,29 +40,6 @@ struct Counts: Decodable {
         case follows
         case followedBy = "followed_by"
     }
-}
-
-/// Данные о пользователе социальной сети
-struct User: Decodable {
-    
-    let id: String
-    let userName: String
-    let fullName: String
-    let avatarURL: String
-    let bio: String
-    let webSite: String
-    let counts: Counts
-    
-    enum CodingKeys: String, CodingKey {
-        case id
-        case userName = "username"
-        case fullName = "full_name"
-        case avatarURL = "profile_picture"
-        case bio
-        case webSite = "website"
-        case counts
-    }
-    
 }
 
 /// Корневой контейнер
