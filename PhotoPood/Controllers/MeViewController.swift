8bb73c5506b63be8a2cee76ecccdf00dd609cb5e
KirillovE <evgeniy.kirillov@live.ru>
2018-10-13 03:29:58 +0300

    На главном экране отображается информация о пользователе

--- PhotoPood/Controllers/MeViewController.swift
+++ PhotoPood/Controllers/MeViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AlamofireImage
 
 class MeViewController: UIViewController {
 
@@ -14,9 +15,36 @@ class MeViewController: UIViewController {
     @IBOutlet weak var followers: UILabel!
     @IBOutlet weak var follows: UILabel!
     @IBOutlet weak var table: UITableView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    let apiManager = APIManager()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         avatar.layer.cornerRadius = avatar.frame.height / 2
+        navigationController?.navigationBar.backgroundColor = .cyan
+        loadUserInformation()
     }
+    
+    private func loadUserInformation() {
+        activityIndicator.startAnimating()
+        
+        apiManager.getUser { user in
+            DispatchQueue.main.async {
+                self.present(user)
+                self.activityIndicator.stopAnimating()
+            }
+        }
+    }
+    
+    private func present(_ user: User?) {
+        guard let user = user else { return }
+        
+        avatar.af_setImage(withURL: URL(string: user.avatarURL)!)
+        follows.text = "подписки\n" + String(user.counts.follows)
+        followers.text = "подписчики\n" + String(user.counts.followedBy)
+        title = "@" + user.username
+    }
+    
 }
