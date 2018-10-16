5e02ba51ace5735ed478fbbc8fff9935faeec4ed
KirillovE <evgeniy.kirillov@live.ru>
2018-10-16 21:20:30 +0300

    Создан класс-заместитель пользователя для работы совместно с фотографиями

---
+++ PhotoPood/Instagram entities/SenderPhoto.swift
@@ -0,0 +1,32 @@
+//
+//  SenderPhoto.swift
+//  PhotoPood
+//
+//  Created by Евгений Кириллов on 16/10/2018.
+//  Copyright © 2018 Триада. All rights reserved.
+//
+
+import Foundation
+
+protocol UserPhoto {
+    var username: String { get }
+    var pictureURL: String { get }
+}
+
+/// Заместитель для класса `User`
+class SenderPhoto: UserPhoto {
+    let owner: User
+    
+    var username: String {
+        return owner.username
+    }
+    
+    var pictureURL: String {
+        return owner.avatarURL
+    }
+    
+    init(with user: User) {
+        owner = user
+    }
+    
+}
