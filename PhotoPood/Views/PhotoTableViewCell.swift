7799fdd45b8d521adc73b101b6c7fe42b92c1aee
KirillovE <evgeniy.kirillov@live.ru>
2018-10-16 20:30:39 +0300

    Создан пользовательский класс табличной ячейки

---
+++ PhotoPood/Views/PhotoTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  PhotoTableViewCell.swift
+//  PhotoPood
+//
+//  Created by Евгений Кириллов on 16/10/2018.
+//  Copyright © 2018 Триада. All rights reserved.
+//
+
+import UIKit
+
+class PhotoTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+}
