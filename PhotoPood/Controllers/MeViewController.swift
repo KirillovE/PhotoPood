9b9f7a6321fa37d5b1220c8acc7daf96a7a57d54
KirillovE <evgeniy.kirillov@live.ru>
2018-10-16 20:32:15 +0300

    На начальном экране отображаются фотографии пользователя

--- PhotoPood/Controllers/MeViewController.swift
+++ PhotoPood/Controllers/MeViewController.swift
@@ -11,21 +11,35 @@ import AlamofireImage
 
 class MeViewController: UIViewController {
 
+    // MARK: - Outlets
+    
     @IBOutlet weak var avatar: UIImageView!
     @IBOutlet weak var followers: UILabel!
     @IBOutlet weak var follows: UILabel!
     @IBOutlet weak var table: UITableView!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     
-    let apiManager = APIManager()
+    // MARK: - Свойства
     
+    let apiManager = APIManager()
+    var photos = [Photo]()
+    
+    // MARK: - Методы общего назначения
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        initialSetup()
+        loadUserInformation()
+        loadPhotos()
+    }
+    
+    private func initialSetup() {
         avatar.layer.cornerRadius = avatar.frame.height / 2
         navigationController?.navigationBar.backgroundColor = .cyan
-        loadUserInformation()
+        table.dataSource = self
     }
+    
+    // MARK: - Отображение информации о пользователе
     
     private func loadUserInformation() {
         activityIndicator.startAnimating()
@@ -38,13 +52,44 @@ class MeViewController: UIViewController {
         }
     }
     
+    private func loadPhotos() {
+        activityIndicator.startAnimating()
+        
+        apiManager.getPhotos { photos in
+            self.photos = photos
+            DispatchQueue.main.async {
+                self.table.reloadData()
+                self.activityIndicator.stopAnimating()
+            }
+        }
+    }
+    
     private func present(_ user: User?) {
         guard let user = user else { return }
         
         avatar.af_setImage(withURL: URL(string: user.avatarURL)!)
-        follows.text = "подписки\n" + String(user.counts.follows)
-        followers.text = "подписчики\n" + String(user.counts.followedBy)
+        follows.text = "подписки\n" + String(user.counts?.follows ?? 0)
+        followers.text = "подписчики\n" + String(user.counts?.followedBy ?? 0)
         title = "@" + user.username
+    }
+    
+}
+
+// MARK: - Расширение для работы с таблицей
+
+extension MeViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return photos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MeTableCell") as! PhotoTableViewCell
+        
+        let photoURL = URL(string: photos[indexPath.row].url)
+        cell.photoImageView.af_setImage(withURL: photoURL!)
+        
+        return cell
     }
     
 }
